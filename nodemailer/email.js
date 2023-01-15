4841452b749906ae0f12d50c6953bd710df2d458
Iskyy <iskycruise01@gmail.com>
2023-01-15 14:48:41 +0700

    changed email message

--- nodemailer/email.js
+++ nodemailer/email.js
@@ -2,6 +2,19 @@ var nodemailer = require("nodemailer");
 
 module.exports.MessageMailer = async function (email, name, checkIn, room) {
   const date = new Date(checkIn);
+
+  const rooms = {
+    luxury: "Luxury room",
+    modern: "Modern residence",
+    compact: "Compact size",
+    families: "For Families",
+    minimal: "Minimal design, maximal comfort",
+  };
+
+  const RoomChoose = (room) => {
+    return rooms[room];
+  };
+
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -14,7 +27,9 @@ module.exports.MessageMailer = async function (email, name, checkIn, room) {
     from: "styles132001@gmail.com",
     to: "psxw2001@gmail.com",
     subject: "MrStudio Hotel",
-    text: `Dear ${name}, Thank you for choosing to stay with us at MrStudio Hotel! We are excited to have you as our guest from ${date.toDateString()}. Room type ${room}`,
+    text: `Dear ${name}, Thank you for choosing to stay with us at MrStudio Hotel! We are excited to have you as our guest from ${date.toDateString()}. Room type "${RoomChoose(
+      room
+    )}"`,
     // html: "<b>Hello world?</b>",
   };
 
